7d33eb19439ceab6e78d8d319c3031e7dde11bda
jesse1901 <jesseschuetz@gmx.de>
2025-04-29 09:41:15 +0200

    add light theme to embed link

--- src/pages/reporting.py
+++ src/pages/reporting.py
@@ -3,4 +3,4 @@ import streamlit.components.v1 as components
 
 
 def show_reporting():
-    components.iframe("https://max-portal.desy.de/reporting?embed=true", height=1200, scrolling=True)
+    components.iframe("https://max-portal.desy.de/reporting?embed=true&embed_options=light_theme", height=1200, scrolling=True)
